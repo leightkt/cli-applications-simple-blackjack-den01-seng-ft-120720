5ad04704cc3c6e7bc060584c7108573c0a2255f8
Kat Leight <leightkt@gmail.com>
2020-10-13 20:58:57 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -44,6 +44,7 @@ def hit?(card_total)
   if input == "h"
     new_card = deal_card
     card_total += new_card
+    display_card_total(card_total)
     card_total
   elsif input == "s"
     card_total
